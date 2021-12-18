abe36b9c6f3ae6f516e53c22850b56249822d273
CoderLawe <52962217+CoderLawe@users.noreply.github.com>
2021-12-18 21:36:11 +0200

    color fix

--- components/Message.js
+++ components/Message.js
@@ -17,7 +17,7 @@ function Message({message}) {
                 <Avatar username ={message.get("username")}/>
             </div>
             <div className={`flex space-x-4 p-3 rounded-lg ${
-                isUserMessage ? "rounded-br-none bg-gradient-to-r from-blue-900 to-black text-white border-2 border-white bg-opacity-75 font-serif":"rounded-bl-none bg-gradient-to-l from-yellow-700 to-black text-black font-serif border-black bg-opacity-75"
+                isUserMessage ? "rounded-br-none bg-gradient-to-r from-blue-900 to-black text-white border-2 border-white bg-opacity-75 font-serif": "rounded-br-none bg-gradient-to-r from-blue-900 to-black text-white border-2 border-white bg-opacity-75 font-serif"
             }`}>
                 <p>{message.get("message")}</p>
             </div>
@@ -26,7 +26,7 @@ function Message({message}) {
 
             className={`text-[10px] italic text-gray-400 `}
             datetime={message.createdAt}/>
-            <p className={`absolute -mb-5 text-xs ${isUserMessage ? "text-pink-300": "text-blue-400"}`}>{message.get("username")}</p>
+            <p className={`absolute -mb-5 text-sm ${isUserMessage ? "text-green-400": "text-blue-400"}`}>{message.get("username")}</p>
         </div>
     )
 }
